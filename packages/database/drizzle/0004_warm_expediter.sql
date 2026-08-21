faeb447b481748612ff4faa52551000ca3e7ef0d
Visshnnu Tejaa <visshnnutejaa@gmail.com>
2026-08-21 14:37:31 +0530

    feat: add telemetry columns to responses table

---
+++ packages/database/drizzle/0004_warm_expediter.sql
@@ -0,0 +1,4 @@
+ALTER TABLE "responses" ADD COLUMN "device" varchar(50);--> statement-breakpoint
+ALTER TABLE "responses" ADD COLUMN "browser" varchar(50);--> statement-breakpoint
+ALTER TABLE "responses" ADD COLUMN "city" varchar(50);--> statement-breakpoint
+ALTER TABLE "responses" ADD COLUMN "country" varchar(50);
