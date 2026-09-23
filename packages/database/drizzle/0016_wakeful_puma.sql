32731f16c35ddd256e5da32f1c6c0f5eb7b7a506
Visshnnu Tejaa <visshnnutejaa@gmail.com>
2026-09-23 17:38:23 +0530

    fix db error

--- packages/database/drizzle/0016_wakeful_puma.sql
+++ packages/database/drizzle/0016_wakeful_puma.sql
@@ -1,5 +1,5 @@
 ALTER TABLE "analytics"
-ADD COLUMN "deleted_at" timestamp with time zone DEFAULT NOT NULL;
+ADD COLUMN "deleted_at" timestamp with time zone;
 --> statement-breakpoint
 ALTER TABLE "form_views"
-ADD COLUMN "deleted_at" timestamp with time zone DEFAULT NOT NULL;
+ADD COLUMN "deleted_at" timestamp with time zone;
